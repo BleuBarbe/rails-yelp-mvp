a29fff75831c6c97f48cf2799bc9ea51191db556
Emmanuel Mack <mack.reboisson@gmail.com>
2022-02-16 15:28:22 +0100

    last

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :rating, :content, presence: true
+  validates :rating, numericality: { only_integer: true}, acceptance: { accept: [0, 1, 2, 3, 4, 5] }
 end
